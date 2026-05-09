720a8e954be7642bedac9157aa2161b5722e4023
Norakneath25 <norakneath230505@gmail.com>
2026-05-09 19:42:31 +0700

    add breeze auth

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,4 +1,6 @@
-import { createInertiaApp } from '@inertiajs/vue3';
+import { createInertiaApp  } from '@inertiajs/vue3';
+import type {DefineComponent} from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import { createApp, h } from 'vue';
@@ -7,13 +9,17 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
+    resolve: (name) =>
+        resolvePageComponent(
+        `./Pages/${name}.vue`,
+        import.meta.glob<DefineComponent>('./Pages/**/*.vue', { eager: true }),
+    ),
     progress: {
         color: '#4B5563',
     },
     setup({ el, App, props, plugin }) {
         const pinia = createPinia();
         pinia.use(piniaPluginPersistedstate);
-
         createApp({
             render: () => h(App, props),
         })
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,37 @@
 <?php
 
+use App\Http\Controllers\PhoneController;
+use App\Http\Controllers\ProfileController;
 use App\Models\Phone;
-use Inertia\Inertia;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
+// Home
 Route::inertia('/', 'Home')->name('home');
-Route::inertia('/compare', 'Compare')->name('compare');
 
-
+// Phone detail
 Route::get('/phones/{id}', function ($id) {
     $phone = Phone::with('reviews')->findOrFail($id);
     return Inertia::render('PhoneDetail', ['phone' => $phone]);
 });
 
-
+// Compare
 Route::get('/compare', function () {
     $phones = Phone::all();
     return Inertia::render('Compare', ['phones' => $phones]);
 })->name('compare');
+
+// Dashboard
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Profile
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
